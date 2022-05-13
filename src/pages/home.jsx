67e474f858e660adeb7532dd07f9ac12f9f7a214
HensleyFerrari <hensley_ferrari@hotmail.com>
2022-05-13 18:54:52 -0300

    Changed button card color

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -30,7 +30,7 @@ function Home({ auth, validateToken }) {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 self-center text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
-          <input type="text" value={searchTerm} placeholder='Quero escutar ...' className='w-full outline outline-offset-1 outline-purple-500 rounded-md' onChange={e => { setSearchTerm(e.target.value) }} />
+          <input type="text" value={searchTerm} placeholder='Quero escutar... (filtro pelo nome da música)' className='w-full outline outline-offset-1 outline-purple-500 rounded-md p-2' onChange={e => { setSearchTerm(e.target.value) }} />
           <button onClick={e => { setSearchTerm("") }} className='px-4 py-2 rounded-md bg-gray-200'>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 self-center" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
